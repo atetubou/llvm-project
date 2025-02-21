a053fd40d86b8d774058ed71b045a005ae7a1b64
Takuto Ikuta <tikuta@google.com>
2025-02-21 14:44:32 +0900

    [libcxx] Remove unnecessary includes from atomic_flag.h

--- libcxx/include/__atomic/atomic_flag.h
+++ libcxx/include/__atomic/atomic_flag.h
@@ -13,10 +13,8 @@
 #include <__atomic/contention_t.h>
 #include <__atomic/memory_order.h>
 #include <__atomic/support.h>
-#include <__chrono/duration.h>
 #include <__config>
 #include <__memory/addressof.h>
-#include <__thread/support.h>
 #include <cstdint>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
